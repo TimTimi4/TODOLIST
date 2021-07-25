236c9bebfb4f47b6f67fb39d8ef733840262ae5f
TimTimi4 <doronin200894@gmail.com>
2021-07-25 20:23:31 +0600

    add task-block

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Модалка
+//======================================================================================================
 const popupOpen = document.querySelector('.todolist__btn')
 const popup = document.querySelector('.popup')
 const popupClose = document.querySelector('.close-popup')
@@ -15,34 +15,67 @@ popupClose.addEventListener('click', () => {
 bgClose.addEventListener('click', () => {
 	popup.classList.remove('open')
 })
-// Модалка
+//======================================================================================================
 
-const taskTitleArr = document.querySelectorAll('.task__title')
-const taskCheckMarkArr = document.querySelectorAll('.task__checkMark')
+//======================================================================================================
+const saveBtn = document.querySelector('.saveBtn')
+const column = document.querySelector('.column')
+const input = document.querySelector('.popup-form__titleField')
+const textarea = document.querySelector('.popup-form__descField')
 
-taskCheckMarkArr[0].addEventListener('click', () => {
-	taskTitleArr[0].classList.toggle('check')
+saveBtn.addEventListener('click', () => {
+	if (input.value != '') {
+
+		const divTask = document.createElement('div')
+		const h6TaskTitle = document.createElement('h6')
+		const divTaskText = document.createElement('div')
+		const inputTaskCheckMark = document.createElement('input')
+		const divTaskTrash = document.createElement('div')
+		const iTrash = document.createElement('i')
+		divTask.classList.add('task', 'column__task')
+		column.prepend(divTask)
+		h6TaskTitle.classList.add('task__title')
+		divTaskText.classList.add('task__text')
+		inputTaskCheckMark.classList.add('task__checkMark')
+		inputTaskCheckMark.setAttribute('type', 'checkbox')
+		inputTaskCheckMark.setAttribute('name', 'checkMark')
+		divTaskTrash.classList.add('task__trash')
+		divTask.prepend(h6TaskTitle, divTaskText, inputTaskCheckMark, divTaskTrash)
+		iTrash.classList.add('fas', 'fa-trash')
+		divTaskTrash.prepend(iTrash)
+
+		const taskTitleArr = document.querySelectorAll('.task__title')
+		const taskTextArr = document.querySelectorAll('.task__text')
+		const taskCheckMarkArr = document.querySelectorAll('.task__checkMark')
+		const inputValue = input.value
+		const textareaValue = textarea.value
+		taskTitleArr[0].innerHTML = inputValue
+		taskTextArr[0].innerHTML = textareaValue
+
+
+
+
+
+		input.value = ''
+		textarea.value = ''
+		popup.classList.remove('open')
+	}
+	else {
+		alert('Заполните поле заголовка')
+	}
 })
-taskCheckMarkArr[1].addEventListener('click', () => {
-	taskTitleArr[1].classList.toggle('check')
-})
-taskCheckMarkArr[2].addEventListener('click', () => {
-	taskTitleArr[2].classList.toggle('check')
-})
-taskCheckMarkArr[3].addEventListener('click', () => {
-	taskTitleArr[3].classList.toggle('check')
-})
-taskCheckMarkArr[4].addEventListener('click', () => {
-	taskTitleArr[4].classList.toggle('check')
-})
-taskCheckMarkArr[5].addEventListener('click', () => {
-	taskTitleArr[5].classList.toggle('check')
-})
+//======================================================================================================
 
 
 
 
+//======================================================================================================
 
+// const taskTitleArr = document.querySelectorAll('.task__title')
+// const taskCheckMarkArr = document.querySelectorAll('.task__checkMark')
 
+// taskCheckMarkArr[0].addEventListener('click', () => {
+// 	taskTitleArr[0].classList.toggle('check')
+// })
 
-
+//======================================================================================================
