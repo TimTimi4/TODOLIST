303651a2689cc96359a6efd9abf86dd99ad7c395
TimTimi4 <doronin200894@gmail.com>
2021-07-26 20:39:29 +0600

    add delete task

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const popupOpen = document.querySelector('.todolist__btn')
 const popup = document.querySelector('.popup')
 const popupClose = document.querySelector('.close-popup')
 const bgClose = document.querySelector('.popup__bg')
+const closeModalBtn = document.querySelector('.closeModalBtn')
+
 
 popupOpen.addEventListener('click', () => {
 	popup.classList.add('open')
@@ -15,14 +17,18 @@ popupClose.addEventListener('click', () => {
 bgClose.addEventListener('click', () => {
 	popup.classList.remove('open')
 })
+
+closeModalBtn.addEventListener('click', () => {
+	popup.classList.remove('open')
+})
+
 //======================================================================================================
 
 //======================================================================================================
 const saveBtn = document.querySelector('.saveBtn')
-const column = document.querySelector('.column')
+const rowTask = document.querySelector('.header-todolist__row')
 const input = document.querySelector('.popup-form__titleField')
 const textarea = document.querySelector('.popup-form__descField')
-
 
 
 
@@ -37,20 +43,21 @@ saveBtn.addEventListener('click', () => {
 		const h6TaskTitle = document.createElement('h6')
 		const divTaskText = document.createElement('div')
 		const inputTaskCheckMark = document.createElement('input')
-		const divTaskTrash = document.createElement('div')
+		const buttonTaskTrash = document.createElement('button')
 		const iTrash = document.createElement('i')
-		divTask.classList.add('task', 'column__task')
+		divTask.classList.add('task', 'row__task')
 		h6TaskTitle.classList.add('task__title')
 		divTaskText.classList.add('task__text')
 		inputTaskCheckMark.classList.add('task__checkMark')
 		inputTaskCheckMark.setAttribute('type', 'checkbox')
 		inputTaskCheckMark.setAttribute('name', 'checkMark')
-		divTaskTrash.classList.add('task__trash')
-		divTask.prepend(h6TaskTitle, divTaskText, inputTaskCheckMark, divTaskTrash)
+		buttonTaskTrash.setAttribute('class', 'task__trash')
+		buttonTaskTrash.setAttribute('type', 'button')
+		divTask.prepend(h6TaskTitle, divTaskText, inputTaskCheckMark, buttonTaskTrash)
 		iTrash.classList.add('fas', 'fa-trash')
-		divTaskTrash.prepend(iTrash)
+		buttonTaskTrash.prepend(iTrash)
 
-		column.prepend(divTask)
+		rowTask.prepend(divTask)
 
 
 
@@ -67,6 +74,46 @@ saveBtn.addEventListener('click', () => {
 		taskCheckMarkArr[0].addEventListener('click', () => {
 			taskTitleArr[0].classList.toggle('check')
 		})
+
+
+		// ! Удаление задачи
+		const trashArr = document.querySelectorAll('.task__trash')
+		const divTaskArr = document.querySelectorAll('.task')
+		console.log(divTaskArr)
+		console.log(trashArr)
+
+
+		const popupDeleteTask = document.querySelector('.popupDeleteTask')
+		const acceptDelete = document.querySelector('.popupDeleteTask__btn')
+		const deniedDelete = document.querySelector('.popupDeleteTask__close')
+		const popupDeleteBg = document.querySelector('.popupDeleteTask__bg')
+
+		for (let i = 0; i < trashArr.length; i++) {
+			trashArr[0].addEventListener('click', () => {
+				popupDeleteTask.classList.add('openDeleteModal')
+				acceptDelete.addEventListener('click', () => {
+					divTaskArr[0].remove()
+					popupDeleteTask.classList.remove('openDeleteModal')
+				})
+				deniedDelete.addEventListener('click', () => {
+					popupDeleteTask.classList.remove('openDeleteModal')
+				})
+				popupDeleteBg.addEventListener('click', () => {
+					popupDeleteTask.classList.remove('openDeleteModal')
+				})
+			})
+		}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
